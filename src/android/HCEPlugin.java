8a586aca10cec343f3aec7869ab04ea68ddf466c
soonann <soonann.tan@outlook.com>
2018-12-22 22:56:53 +0800

    added hardcoded values for testing

--- src/android/HCEPlugin.java
+++ src/android/HCEPlugin.java
@@ -41,7 +41,8 @@ public class HCEPlugin extends CordovaPlugin {
 
         } else if (action.equalsIgnoreCase(SEND_RESPONSE)) {
 
-            byte[] data = args.getArrayBuffer(0);
+            byte[] data = ConcatArrays("123".getBytes(), HexStringToByteArray("9000")); 
+			Log.d(TAG, " CordovaData:" + Arrays.toString(data) );
 
             if (CordovaApduService.sendResponse(data)) {
                 callbackContext.success();
@@ -83,5 +84,47 @@ public class HCEPlugin extends CordovaPlugin {
             result.setKeepCallback(true);
             onCommandCallback.sendPluginResult(result);
         }
+    }
+
+
+    public static String ByteArrayToHexString(byte[] bytes) {
+        final char[] hexArray = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        char[] hexChars = new char[bytes.length * 2]; // Each byte has two hex characters (nibbles)
+        int v;
+        for (int j = 0; j < bytes.length; j++) {
+            v = bytes[j] & 0xFF; // Cast bytes[j] to int, treating as unsigned value
+            hexChars[j * 2] = hexArray[v >>> 4]; // Select hex character from upper nibble
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F]; // Select hex character from lower nibble
+        }
+        return new String(hexChars);
+    }
+
+
+    public static byte[] HexStringToByteArray(String s) throws IllegalArgumentException {
+        int len = s.length();
+        if (len % 2 == 1) {
+            throw new IllegalArgumentException("Hex string must have even number of characters");
+        }
+        byte[] data = new byte[len / 2]; // Allocate 1 byte per 2 hex characters
+        for (int i = 0; i < len; i += 2) {
+            // Convert each character into a integer (base-16), then bit-shift into place
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                    + Character.digit(s.charAt(i+1), 16));
+        }
+        return data;
+    }
+
+    public static byte[] ConcatArrays(byte[] first, byte[]... rest) {
+        int totalLength = first.length;
+        for (byte[] array : rest) {
+            totalLength += array.length;
+        }
+        byte[] result = Arrays.copyOf(first, totalLength);
+        int offset = first.length;
+        for (byte[] array : rest) {
+            System.arraycopy(array, 0, result, offset, array.length);
+            offset += array.length;
+        }
+        return result;
     }
 }
